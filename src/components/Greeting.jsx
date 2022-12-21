a4db8b44ca0bfe9dcc10252a8dc9a519c6fbc3f1
deesclouds <deescloud@protonmail.com>
2022-12-21 10:22:53 -0800

    removed thank you for visiting greeting! from the component

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -8,10 +8,10 @@ export default function Greeting({messages} ) {
 
     return (
         <div>
-            <h3>{greeting}! Thank you for visiting!</h3>
+            <h3>{greeting}!</h3>
             <button onClick={() => setGreeting(randomMessage())}>
             
-                New Greeting
+                Get New Greeting
             </button>
         </div>
     );
